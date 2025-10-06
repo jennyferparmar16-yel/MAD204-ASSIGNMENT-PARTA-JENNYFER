9092b852890abef8fe50f475fd68ffe1b8c7697c
jennyferparmar16-yel <jenniparmar7@gmail.com>
2025-10-06 16:01:58 -0400

    Evaluable class and PeopleManagementSystem class
    Evaluable.java
     Description:
     - Interface representing entities whose performance can be evaluated.
     - Classes that implement this interface must define the
     - evaluatePerformance() method.
     - Implemented by: Student, Professor, TeachingAssistant
    
    PeopleManagementSystem class
     Description:
     - Main class that manages a collection of Person objects, including Students, Professors, and Teaching Assistants.
     - Provides a menu-driven console interface for adding, listing, searching, removing, and evaluating people.
     - Demonstrates Object-Oriented Programming principles such as inheritance, polymorphism, abstraction, interfaces, and method overloading.
     -Includes recursion (factorial and countdown), exception handling,
     - input validation, and file I/O for data persistence.

---
+++ Evaluable.java
@@ -0,0 +1,18 @@
+/**
+ Evaluable.java
+ Description:
+ - Interface representing entities whose performance can be evaluated.
+ - Classes that implement this interface must define the
+ - evaluatePerformance() method.
+
+ - Implemented by: Student, Professor, TeachingAssistant
+
+  Jennyfer Parmar
+  Date: 06/10/2025
+ */
+
+
+public interface Evaluable
+{
+    String evaluatePerformance();
+}
